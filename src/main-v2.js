a74e881231650f58622c868f39054ee780c8eaf1
Kuro <kuroguo@qq.com>
2018-05-08 11:15:48 +0800

    diff adjust

--- src/main-v2.js
+++ src/main-v2.js
@@ -1,4 +1,8 @@
 const SHA256 = require("crypto-js/sha256");
+const BigNumber = require('bignumber.js')
+const { performance } = require('perf_hooks')
+
+let diffy = new BigNumber('2', 10)
 
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
@@ -15,20 +19,23 @@ class Block {
     }
 
     mineBlock(difficulty) {
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
+      // console.log(diffy.toString(16))
+        // console.log(new BigNumber(Array(64 + 1).join('f'), 16).minus(diffy).toString(16))
+        while (!new BigNumber(this.hash, 16).isLessThan(new BigNumber(Array(64 + 1).join('f'), 16).minus(diffy))) {
+          // console.log(this.hash)
+          // console.log(hex2bin(this.hash))
+          this.nonce++
+          this.hash = this.calculateHash()
         }
 
-        console.log("BLOCK MINED: " + this.hash);
+        console.log(`新出块 ${this.index}: ${this.hash}`);
     }
 }
-
 
 class Blockchain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 5;
+        this.difficulty = diffy
     }
 
     createGenesisBlock() {
@@ -64,8 +71,33 @@ class Blockchain{
 }
 
 let savjeeCoin = new Blockchain();
-console.log('Mining block 1...');
-savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
 
-console.log('Mining block 2...');
-savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+let startTime, spend, df = 10000, spends = []
+
+for (var i = new BigNumber('0', 10); i.isLessThan(new BigNumber(Array(64 + 1).join('f'), 16)); i = i.plus(1)) {
+  // console.log(`Mining block ${i.toString(10)}...`, difficulty.toString(16))
+  diffy = new BigNumber(Array(64 + 1).join('f'), 16).times(1 - 1 / df)
+  console.log('难度: ', df)
+  startTime = performance.now()
+  savjeeCoin.addBlock(new Block(i, "20/07/2017", { amount: i.toString(10) }))
+  spend = (performance.now() - startTime) / 1000 || 0.001
+  spends.push(spend)
+  // if (spends.length > 100) spends.shift()
+  console.log('花费时间：',  `${spend} 秒`)
+
+  let avg = spends.slice(spends.length - 10).reduce((a, b) => a + b) / 10
+  // let avg = spends.reduce((a, b) => a + b) / spends.length
+  let targetTime = 1
+  let times = targetTime / avg
+  if (times > 1.01) {
+    times = 1.01
+  } else if (times < 0.99) {
+    times = 0.99
+  }
+  df *= times
+  console.log('平均花费时间：', `${spends.reduce((a, b) => a + b) / spends.length} 秒`)
+  console.log('目标时间：', `${targetTime} 秒`)
+  console.log('难度乘以：', times || 1)
+
+  console.log('\r\n')
+}
